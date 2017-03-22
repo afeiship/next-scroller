6ce9a076810a41ced94371b16fe6afc55610aa04
afeiship <1290657123@qq.com>
2017-03-22 10:57:09 +0800

    initial

---
+++ gulp/build.js
@@ -0,0 +1,23 @@
+(function() {
+
+  'use strict';
+
+  var gulp = require('gulp');
+  var config = require('./config');
+  var argv = require('yargs').argv;
+  var $ = require('gulp-load-plugins')({
+    pattern: ['gulp-*', 'gulp.*', 'del']
+  });
+
+  gulp.task('build', ['clean'], function() {
+    return gulp.src('src/*.js')
+      .pipe(gulp.dest('dist'))
+      .pipe($.size({title:'[ default size ]:'}))
+      .pipe($.uglify())
+      .pipe($.rename({
+        extname: '.min.js'
+      }))
+      .pipe(gulp.dest('dist'));
+  });
+
+}());
